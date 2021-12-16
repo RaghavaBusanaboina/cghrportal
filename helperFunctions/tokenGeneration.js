71150ed367fc45859c951dafb0a6bbfc9f23bd49
RaghavaBusanaboina <busanaboinaraghava@gmail.com>
2021-12-16 17:26:35 +0530

    ntg

--- helperFunctions/tokenGeneration.js
+++ helperFunctions/tokenGeneration.js
@@ -13,39 +13,41 @@ var createToken = async function (
   Password,
   organisation
 ) {
-  var ip =
-    (req.headers["x-forwarded-for"] || "").split(",").pop().trim() ||
-    req.socket.remoteAddress;
+  try {
+    var ip = req.socket.remoteAddress; //(req.headers["x-forwarded-for"] || "").split(",").pop().trim() ||
 
-  const user_logins = await EmployeeLogin.find({
-    EmployeeId: EmployeeId,
-    token_deleted: false,
-    ip_address: ip,
-    device: device,
-  });
-  user_logins.forEach(async (login) => {
-    if (login) {
-      login.token_deleted = true;
-      await login.save();
-    }
-  });
-
-  const token = await new EmployeeLogin({
-    EmployeeId: EmployeeId,
-    ip_address: ip,
-    device: device,
-  });
-
-  const accessToken = await jwt.sign(
-    {
+    const user_logins = await EmployeeLogin.find({
       EmployeeId: EmployeeId,
-      EmployeeName: EmployeeName,
-      Password: Password,
-      organisation: organisation,
-    },
-    config.get("jwtPrivateKey")
-  );
-  return accessToken;
-};
+      token_deleted: false,
+      ip_address: ip,
+      device: device,
+    });
+    user_logins.forEach(async (login) => {
+      if (login) {
+        login.token_deleted = true;
+        await login.save();
+      }
+    });
 
-exports.createToken = createToken;
+    const token = await new EmployeeLogin({
+      EmployeeId: EmployeeId,
+      ip_address: ip,
+      device: device,
+    });
+
+    const accessToken = await jwt.sign(
+      {
+        EmployeeId: EmployeeId,
+        EmployeeName: EmployeeName,
+        Password: Password,
+        organisation: organisation,
+      },
+      config.get("jwtPrivateKey")
+    );
+    return accessToken;
+
+    exports.createToken = createToken;
+  } catch (error) {
+    console.log(new Error(error));
+  }
+};

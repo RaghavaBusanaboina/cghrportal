8b272cb0dcc8c8d42de816e26cba434f25f9db69
RaghavaBusanaboina <busanaboinaraghava@gmail.com>
2021-12-17 12:37:55 +0530

    1

--- middlewares/authemp.js
+++ middlewares/authemp.js
@@ -1,20 +1,74 @@
 /** @format */
 
 const jwt = require("jsonwebtoken");
-// const config = require("config");
+const config = require("config");
+const { EmployeeLogin } = require("../models/userLogin");
+const { TokenBlackList } = require("../models/blackListToken");
 
-module.exports = function (req, res, next) {
-  const token = req.header("x-auth-token");
+// module.exports = function (req, res, next) {
+//   const token = req.header("x-auth-token");
+//   if (!token)
+//     return res.status(401).send("Access denied. No token providedae.");
+//   try {
+//     const decoded = jwt.verify(token, "PrivateKey");
+//     console.log(decoded);
+//     req.user = decoded;
+//     next();
+//     return decoded;
+//   } catch (ex) {
+//     console.log(`${ex}`);
+//     res.status(400).send(`issueeeeeee${ex}`); //("Session Expired! Please Login Again");
+//   }
+// };
+
+module.exports = async function (req, res, next) {
+  const token = req.headers["x-auth-token"];
   if (!token)
-    return res.status(401).send("Access denied. No token providedae.");
-  try {
-    const decoded = jwt.verify(token, "PrivateKey");
-    console.log(decoded);
-    req.user = decoded;
-    next();
-    return decoded;
-  } catch (ex) {
-    console.log(`${ex}`);
-    res.status(400).send(`issueeeeeee${ex}`); //("Session Expired! Please Login Again");
-  }
+    return res.status(404).send("Access denied. No token providedae.");
+  TokenBlackList.findOne({ token: token }).then((found) => {
+    if (found) {
+      jwt.verify(token, config.get("jwtPrivateKey"), async (err, payload) => {
+        const login = await EmployeeLogin.findOne({
+          EmployeeId: payload.EmployeeId,
+          token_id: payload.token_id,
+        });
+        login.logged_out = true;
+        login.token_deleted = true;
+        await login.save();
+      });
+      details = {
+        Status: "Failure",
+        Details: "Token blacklisted. Cannot use this token.",
+      };
+
+      return res.status(401).json(details);
+    } else {
+      jwt.verify(token, config.get("jwtPrivateKey"), async (err, payload) => {
+        if (err) return res.sendStatus(403);
+        if (payload) {
+          const login = await EmployeeLogin.findOne({
+            EmployeeId: payload.EmployeeId,
+            token_id: payload.token_id,
+          });
+          if (login.token_deleted == true) {
+            login.logged_out = true;
+            await login.save();
+            const blacklist_token = new TokenBlackList({
+              token: token,
+            });
+            blacklist_token.save();
+          } else {
+            login.logged_out = true;
+            login.token_deleted = true;
+            await login.save();
+            const blacklist_token = new TokenBlackList({
+              token: token,
+            });
+            blacklist_token.save();
+          }
+        }
+        next();
+      });
+    }
+  });
 };

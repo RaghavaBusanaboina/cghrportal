fde0c6f11b77a5c038f7b08c2df2ee283d13780c
RaghavaBusanaboina <busanaboinaraghava@gmail.com>
2021-12-21 10:50:33 +0530

    validation chnaged

--- models/employeeRegisters.js
+++ models/employeeRegisters.js
@@ -235,10 +235,15 @@ try {
       LastName: Joi.string().min(3).required(),
       fatherName: Joi.string().min(3).required(),
       motherName: Joi.string().min(3).required(),
-      emergencyNumber: Joi.number().required(),
+      emergencyNumber: Joi.string()
+        .minlength(10)
+        .pattern(/^[6-9]{1}[0-9]{9}$/)
+        .required(),
       Address: Joi.string().min(3).required(),
       City: Joi.string().min(3).required(),
-      Pincode: Joi.number().minlength(6).required(),
+      Pincode: Joi.string()
+        .pattern(/^[0-9]{6}$/)
+        .required(),
       emergencyAddress: Joi.string().min(3).required(),
       AboutMe: Joi.string().min(3).required(),
       last_updated_on: Joi.date(),

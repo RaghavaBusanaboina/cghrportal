18a5c6e239db95fa875cd9f7b870fa67ea523ac4
RaghavaBusanaboina <busanaboinaraghava@gmail.com>
2021-12-21 12:57:29 +0530

    v

--- routes/postemployee.js
+++ routes/postemployee.js
@@ -420,6 +420,8 @@ router.post("/add/:name", authemp, async (req, res, next) => {
 //update educational details..!!1123
 router.post("/updateEducationalDetails/:name", authemp, async (req, res) => {
   const data = req.body;
+  data["percentage"] = Number(data["percentage"]);
+  data["passedoutYear"] = Number(data["passedoutYear"]);
   data["last_updated_on"] = new Date(Date.now());
   console.log("hello1111");
   console.log("printing data1");

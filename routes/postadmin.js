c0b965eb43e85636805242689236eeb33937f08c
RaghavaBusanaboina <busanaboinaraghava@gmail.com>
2021-12-28 15:49:09 +0530

    1

--- routes/postadmin.js
+++ routes/postadmin.js
@@ -276,9 +276,9 @@ router.post("/deleteHolidays", auth, async (req, res) => {
     console.log("data", req.body);
     const query = { organisation: req.body.organisation };
     var index = req.body.index;
-    const update = { "holidays.0": "" };
+    const update = { "holidays.1": "" };
     const del_holidays = await Holidays.findOneAndUpdate(query, {
-      $unset: update,
+      $pull: update,
     });
     console.log("del_holidays");
     return res.status(200).send({ data: del_holidays });

0bc1c4ff212c7f79a5e64923e06669465d485ed5
RaghavaBusanaboina <busanaboinaraghava@gmail.com>
2022-01-03 11:22:22 +0530

    removed redis week n month

--- routes/getadmin.js
+++ routes/getadmin.js
@@ -137,124 +137,124 @@ router.get("/companytimings", both, async (req, res) => {
   return res.status(200).send({ data: timings });
 });
 // //get production hours for all employees for the last 7 and last 30days
-// router.post("/productionhours/week&month", auth, async (req, res) => {
-//   try {
-//     let date = new Date();
-//     let currentYear = date.getFullYear();
-//     let currentMonth = date.getMonth();
-//     let currentDate = date.getDate();
-//     let weekWorkingDays = [];
-//     let monthWorkingDays = [];
-//     async function calculateWorkingingHours(query, type) {
-//       const empattendance = await EmployeeAttendance.find(query);
-//       console.log(empattendance);
-//       if (type === "week") {
-//         weekWorkingDays.push(empattendance.length);
-//       }
-//       if (type === "month") {
-//         monthWorkingDays.push(empattendance.length);
-//       }
-//       if (!empattendance)
-//         return res.status(404).send({ data: "no data found" });
-//       var hours = [];
-//       for (let i = 0; i < empattendance.length; i++) {
-//         const element = empattendance[i];
-//         let TotalWorkingHours = calWorkingHours(
-//           element.inTime,
-//           element.outTime
-//         );
-//         hours.push(TotalWorkingHours);
-//       }
-//       let hoursmins = totalHoursMins(hours);
-//       return hoursmins;
-//     }
-//     const empData = await EmployeeRegisters.find({
-//       organisation: req.user.organisation,
-//     });
-//     var countlen = empData.length;
-//     let finaldata = {};
-//     let total = {};
-//     let totalLastWeekHours = [];
-//     let totalLastMonthHours = [];
-//     for (let i = 0; i < empData.length; i++) {
-//       let weekquery = {
-//         EmployeeId: empData[i].EmployeeId, //
-//         organisation: req.user.organisation,
-//         inTime: { $nin: ["pending", "Holiday"] },
-//         outTime: { $nin: ["pending", "Holiday"] },
-
-//         ADate: {
-//           $gte: new Date(currentYear, currentMonth, currentDate - 8),
-//           $lt: new Date(currentYear, currentMonth, currentDate),
-//         },
-//       };
-//       let monthquery = {
-//         EmployeeId: empData[i].EmployeeId, //
-//         organisation: req.user.organisation,
-//         inTime: { $nin: ["pending", "Holiday"] },
-//         outTime: { $nin: ["pending", "Holiday"] },
-
-//         ADate: {
-//           $gte: new Date(currentYear, currentMonth - 1, 01),
-//           $lt: new Date(currentYear, currentMonth + 1, 01),
-//         },
-//       };
-//       const lastWeekHours = await calculateWorkingingHours(
-//         (query = weekquery),
-//         (type = "week")
-//       );
-//       totalLastWeekHours.push(lastWeekHours);
-//       const lastMonthHours = await calculateWorkingingHours(
-//         (query = monthquery),
-//         (type = "month")
-//       );
-//       totalLastMonthHours.push(lastMonthHours);
-
-//       finaldata[empData[i].EmployeeId] = {
-//         EmployeId: empData[i].EmployeeId,
-//         lastWeekHours: `${lastWeekHours.split(":")[0]}hrs ${
-//           lastWeekHours.split(":")[1]
-//         } mins`,
-//         lastMonthHours: `${lastMonthHours.split(":")[0]}hrs ${
-//           lastMonthHours.split(":")[1]
-//         } mins`,
-//         weekWorkingdays: weekWorkingDays[i],
-//         monthWorkingdays: monthWorkingDays[i],
-//       };
-//     }
-//     var thmweek = totalHoursMins(totalLastWeekHours);
-//     var thmmonth = totalHoursMins(totalLastMonthHours);
-//     total["totalLastWeekHours"] = `${Math.round(
-//       eval(thmweek.split(":")[0]) / countlen
-//     )}hrs ${Math.round(eval(thmweek.split(":")[1]) / countlen)} mins`;
-//     total["totalLastMonthHours"] = `${Math.round(
-//       eval(thmmonth.split(":")[0]) / countlen
-//     )}hrs ${Math.round(eval(thmmonth.split(":")[1]) / countlen)} mins`;
-//     return res
-//       .status(200)
-//       .send({ finaldata, total, count: `${empData.length}` });
-//     // .send({ finaldata: finaldata, totalHours: total, count: empData.length });
-//   } catch (error) {
-//     console.log(`${error}`);
-//     res.status(400).send({ data: error });
-//   }
-// });
-
-//get production hours for all employees for the last 7 and last 30days
 router.post("/productionhours/week&month", auth, async (req, res) => {
   try {
-    await redisset.getweekmonth((organisation = req.user.organisation));
-    var value = await redisget.getweekmonth();
-    if (value === {} || value === null) {
-      await redisset.getweekmonth((organisation = req.user.organisation));
+    let date = new Date();
+    let currentYear = date.getFullYear();
+    let currentMonth = date.getMonth();
+    let currentDate = date.getDate();
+    let weekWorkingDays = [];
+    let monthWorkingDays = [];
+    async function calculateWorkingingHours(query, type) {
+      const empattendance = await EmployeeAttendance.find(query);
+      console.log(empattendance);
+      if (type === "week") {
+        weekWorkingDays.push(empattendance.length);
+      }
+      if (type === "month") {
+        monthWorkingDays.push(empattendance.length);
+      }
+      if (!empattendance)
+        return res.status(404).send({ data: "no data found" });
+      var hours = [];
+      for (let i = 0; i < empattendance.length; i++) {
+        const element = empattendance[i];
+        let TotalWorkingHours = calWorkingHours(
+          element.inTime,
+          element.outTime
+        );
+        hours.push(TotalWorkingHours);
+      }
+      let hoursmins = totalHoursMins(hours);
+      return hoursmins;
     }
-    if ("data" in value) return res.status(400).send(value);
-    return res.status(200).send(value);
+    const empData = await EmployeeRegisters.find({
+      organisation: req.user.organisation,
+    });
+    var countlen = empData.length;
+    let finaldata = {};
+    let total = {};
+    let totalLastWeekHours = [];
+    let totalLastMonthHours = [];
+    for (let i = 0; i < empData.length; i++) {
+      let weekquery = {
+        EmployeeId: empData[i].EmployeeId, //
+        organisation: req.user.organisation,
+        inTime: { $nin: ["pending", "Holiday"] },
+        outTime: { $nin: ["pending", "Holiday"] },
+
+        ADate: {
+          $gte: new Date(currentYear, currentMonth, currentDate - 8),
+          $lt: new Date(currentYear, currentMonth, currentDate),
+        },
+      };
+      let monthquery = {
+        EmployeeId: empData[i].EmployeeId, //
+        organisation: req.user.organisation,
+        inTime: { $nin: ["pending", "Holiday"] },
+        outTime: { $nin: ["pending", "Holiday"] },
+
+        ADate: {
+          $gte: new Date(currentYear, currentMonth - 1, 01),
+          $lt: new Date(currentYear, currentMonth + 1, 01),
+        },
+      };
+      const lastWeekHours = await calculateWorkingingHours(
+        (query = weekquery),
+        (type = "week")
+      );
+      totalLastWeekHours.push(lastWeekHours);
+      const lastMonthHours = await calculateWorkingingHours(
+        (query = monthquery),
+        (type = "month")
+      );
+      totalLastMonthHours.push(lastMonthHours);
+
+      finaldata[empData[i].EmployeeId] = {
+        EmployeId: empData[i].EmployeeId,
+        lastWeekHours: `${lastWeekHours.split(":")[0]}hrs ${
+          lastWeekHours.split(":")[1]
+        } mins`,
+        lastMonthHours: `${lastMonthHours.split(":")[0]}hrs ${
+          lastMonthHours.split(":")[1]
+        } mins`,
+        weekWorkingdays: weekWorkingDays[i],
+        monthWorkingdays: monthWorkingDays[i],
+      };
+    }
+    var thmweek = totalHoursMins(totalLastWeekHours);
+    var thmmonth = totalHoursMins(totalLastMonthHours);
+    total["totalLastWeekHours"] = `${Math.round(
+      eval(thmweek.split(":")[0]) / countlen
+    )}hrs ${Math.round(eval(thmweek.split(":")[1]) / countlen)} mins`;
+    total["totalLastMonthHours"] = `${Math.round(
+      eval(thmmonth.split(":")[0]) / countlen
+    )}hrs ${Math.round(eval(thmmonth.split(":")[1]) / countlen)} mins`;
+    return res
+      .status(200)
+      .send({ finaldata, total, count: `${empData.length}` });
+    // .send({ finaldata: finaldata, totalHours: total, count: empData.length });
   } catch (error) {
     console.log(`${error}`);
     res.status(400).send({ data: error });
   }
 });
+
+//get production hours for all employees for the last 7 and last 30days
+// router.post("/productionhours/week&month", auth, async (req, res) => {
+//   try {
+//     await redisset.getweekmonth((organisation = req.user.organisation));
+//     var value = await redisget.getweekmonth();
+//     if (value === {} || value === null) {
+//       await redisset.getweekmonth((organisation = req.user.organisation));
+//     }
+//     if ("data" in value) return res.status(400).send(value);
+//     return res.status(200).send(value);
+//   } catch (error) {
+//     console.log(`${error}`);
+//     res.status(400).send({ data: error });
+//   }
+// });
 
 //get Employee termination
 router.post("/getemployeetermination", auth, async (req, res) => {
